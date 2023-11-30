3e8cfddac79834f2aa242cb8b41c5c0f1aa621ea
Alex Miroshnichenko <miroshnichenko.ua@gmail.com>
2023-11-30 22:23:30 +0200

    Add correct responsive section reviews

--- js/swiper.js
+++ js/swiper.js
@@ -1,7 +1,8 @@
 const reviewsSwiper = new Swiper('.reviews-swiper', {
   loop: true,
   navigation: {
-    nextEl: '.reviews-btn-next',
-    prevEl: '.reviews-btn-prev',
+    nextEl: '.reviews-btn-prev',
+    prevEl: '.reviews-btn-next',
   },
+  speed: 1000,
 });
